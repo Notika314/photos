b80db5c79d134a3024065ef9076009d42a988819
notika0314 <nb631@scarletmail.rutgers.edu>
2019-11-15 17:17:50 -0500

    picture change

--- src/model/Picture.java
+++ src/model/Picture.java
@@ -1,5 +1,21 @@
 package model;
+import java.io.*;
+import java.util.ArrayList;
 
-public class Picture {
-
+public class Picture implements Serializable{
+	public String path;
+	public User user;
+	ArrayList<String> tags;
+	public Picture(User user, String path) {
+		this.user = user;
+		this.path = path;
+		this.tags = new ArrayList<String>();
+	}
+	
+	public boolean equals(Object o) {
+		if (!( o instanceof Picture)) return false;
+		Picture p = (Picture) o;
+		if (p.path.equals(this.path)) return true;
+		else return false;
+	}
 }
