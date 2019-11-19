afdbebc365e580ba270a6c2dbc9f27e22fbe555a
notika0314 <nb631@scarletmail.rutgers.edu>
2019-11-19 09:33:32 -0500

    picture controller

--- src/controller/PictureController.java
+++ src/controller/PictureController.java
@@ -1,5 +1,24 @@
 package controller;
 
-public class PictureController {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
+
+public class PictureController {
+	@FXML ImageView image;
+	
+	public void start(Stage mainStage) throws FileNotFoundException,IOException {  
+		
+	}
 }
