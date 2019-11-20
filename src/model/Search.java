6017f83675e9182f4f7719817801912347925656
notika0314 <nb631@scarletmail.rutgers.edu>
2019-11-19 22:48:00 -0500

    changed method in search to search by date range instead just by date

--- src/model/Search.java
+++ src/model/Search.java
@@ -6,24 +6,24 @@ import java.util.ArrayList;
 public class Search {
 	static ArrayList<Picture> searchResult;
 	
-	static ArrayList<Picture> searchByDateInAllPhotos(Date date) {
+	static ArrayList<Picture> searchByDateRangeInAlbum(Album a, Date min,Date max) {
 		Search.searchResult = new ArrayList<Picture>();
+		for (int i=0;i<a.pictures.size();i++) {
+			Date picDate = a.pictures.get(i).date;
+			if (picDate.compareTo(min)>=0 && picDate.compareTo(max)<=0) Search.searchResult.add(a.pictures.get(i));
+		}
+		return searchResult;
+	}
+	
+	static ArrayList<Picture> searchByDateRangeInAll(Date min, Date max) {
+		searchResult = new ArrayList<Picture>();
 		for (int i=0;i<User.curr.userAlbums.size();i++) {
 			Album a = User.curr.userAlbums.get(i);
 			for (int j=0;j<a.pictures.size();j++) {
 				Picture p = a.pictures.get(j);
-				if (p.date.equals(date)) Search.searchResult.add(p);
+				if (p.date.compareTo(min)>=0 && p.date.compareTo(max)<=0) Search.searchResult.add(p);
 			}
 		}
 		return searchResult;
 	}
-	
-	static ArrayList<Picture> searchByDateInAnAlbum(Album a, Date date) {
-		Search.searchResult = new ArrayList<Picture>();
-		for (int i=0;i<a.pictures.size();i++) {
-			if (a.pictures.get(i).date.equals(date)) Search.searchResult.add(a.pictures.get(i));
-		}
-		return searchResult;
-	}
-	
 }
